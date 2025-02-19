45a91b60d2917d461a78ac3f7fd283d303cc142c
Arjun-Walia <arjunwalia957@gmail.com>
2025-02-19 13:33:10 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, redirect, request
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['Portfolio']
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/contactus', methods=['POST'])
+def contactus():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+    
+    doc = {
+        'name': name,
+        'email': email,
+        'message': message
+    }
+    
+    db.contactus.insert_one(doc)
+    
+    return "Thanks for contacting"
+
+if __name__=='__main__':
+    app.run(debug=True, port=5000)
